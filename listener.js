5cf868a904b98d8e837e0ea9bce17b3aaf4326a1
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-06-23 16:04:33 +0200

    Add listener.js

---
+++ listener.js
@@ -0,0 +1,25 @@
+export const listener = (target={}) => {
+	let callbacks = new Map()
+	function listen(prop, callback) {
+		if ("object" == typeof prop && "forEach" in prop)
+			prop.forEach(prop => this.listen(prop, callback))
+		else if (callback)
+			callbacks.set(prop, callback)
+		else
+			callbacks.delete(prop)
+	}
+	let proxy = new Proxy(target, {
+		set: (target, prop, value) => {
+			if (callbacks.has("*")) callbacks.get("*")(value, prop)
+			if (callbacks.has(prop)) callbacks.get(prop)(value, prop)
+			return Reflect.set(target, prop, value)
+		},
+		get: (target, prop, value) => {
+			if (prop == "listen")
+				return listen
+			else
+				return Reflect.get(target, prop)
+		}
+	})
+	return proxy
+}
