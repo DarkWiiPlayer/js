7a061b10d58d6cd5d3756cba6936f98b7697f02f
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-09-13 18:54:32 +0200

    Add speaker class, completing the black hand

---
+++ speaker.js
@@ -0,0 +1,37 @@
+class Speaker {
+	#callbacks = new Set()
+	#immediate
+	#scheduled = []
+
+	constructor(immediate) {
+		this.#immediate = immediate
+	}
+
+	listen(callback) {
+		this.#callbacks.add(callback)
+	}
+
+	speak(...args) {
+		if (this.#immediate) {
+			for (let callback of this.#callbacks) {
+				callback(...args)
+			}
+		} else {
+			if (!this.#scheduled.length) {
+				queueMicrotask(() => {
+					for (let callback of this.#callbacks) {
+						callback(...args)
+					}
+					this.#scheduled = []
+				})
+			}
+			this.#scheduled.push(args)
+		}
+	}
+
+	silence(callback) {
+		this.#callbacks.delete(callbacks)
+	}
+}
+
+export default Speaker
