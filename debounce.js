364ada6a45835e0cfc122d1f46b79e06e1ed0894
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-02-08 15:15:19 +0100

    Add cancellability to debounce wrapper

--- debounce.js
+++ debounce.js
@@ -1,10 +1,19 @@
 export default (action, delay=1e3) => {
 	let timeout
-	return (...args) => {
+	const func = (...args) => {
 		if (timeout) clearTimeout(timeout)
 		timeout = setTimeout(() => {
 			timeout = undefined
 			action(...args)
 		}, delay)
 	}
+	func.cancel = () => {
+		if (timeout) clearTimeout(timeout)
+		timeout = undefined
+	}
+	func.now = (...args) => {
+		func.cancel()
+		return action(...args)
+	}
+	return func
 }
